8de1b001eb95e55e944299d71c0b82be4193ef18
kingajay007 <ajay.a338@gmail.com>
2015-11-06 09:21:39 +0530

    master

---
+++ MvcAzureAppOnPremiseWcfTest/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MvcAzureAppOnPremiseWcfTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAzureAppOnPremiseWcfTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult GetStudentById()
+        {
+            Student stu = new Student();
+            try
+            {
+                StudentService.StudentServiceClient client = new StudentService.StudentServiceClient();
+                
+                StudentService.StudentContract contract = client.GetStudentById(1);
+                stu.StudentId = contract.StudentId;
+                stu.StudentName = contract.StudentName;
+                return View(stu);
+            }
+            catch (Exception ex)
+            {
+                stu.StudentId = 0;
+                stu.StudentName = "Student name not found... "+ex.Message;
+                return View();
+            }
+            
+
+            
+        }
+    }
+}
